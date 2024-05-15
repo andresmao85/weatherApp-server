1b959fa6b7f0aecd96d284fdafe93584b39d4d38
Andres <andrex.ao@gmail.com>
2024-05-15 07:41:43 -0500

    refactor: updated server port

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ function parseHourlyWeather({ forecast, current }) {
 }
 
 // app.listen(3001)
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
