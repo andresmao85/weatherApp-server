d10127b0c9b9291472ee7798fb0f2134a5da89c9
Andres <andrex.ao@gmail.com>
2024-05-15 11:39:10 -0500

    refactor: updated cors options origin

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ require("dotenv").config()
 const app = express()
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  // origin: "https://client-opal-rho.vercel.app/",
+  origin: "https://client-opal-rho.vercel.app/",
   optionsSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
